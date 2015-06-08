475cf5450461a2ffb8dcd0f36f809b5b3c824dc9
KeinT <kienpc81089@gmail.com>
2015-06-08 10:52:32 +0700

    Coding style update.

--- includes/class-st-user-activator.php
+++ includes/class-st-user-activator.php
@@ -1,15 +1,4 @@
 <?php
-
-/**
- * Fired during plugin activation
- *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    ST_User
- * @subpackage ST_User/includes
- */
-
 /**
  * Fired during plugin activation.
  *
@@ -28,42 +17,42 @@ class ST_User_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-        $account_slug = 'account';
+        $account_slug   = 'account';
         $shortcode_base = 'st_user';
         $page = array(
-            'post_title' =>__('Account','st-user'),
-            'post_name' =>$account_slug,
-            'post_type' =>'page',
-            'post_content' =>'['.$shortcode_base.']',
+            'post_title'   => __('Account','st-user'),
+            'post_name'    => $account_slug,
+            'post_type'    => 'page',
+            'post_content' => '['.$shortcode_base.']',
 
         );
         $page_id = 0;
-        $p = get_page_by_path(  $account_slug, OBJECT, 'page' );
-        if( $p ){
+        $p = get_page_by_path( $account_slug, OBJECT, 'page' );
+        if ( $p ) {
             $page_id = $p->ID;
 
-            if( ! has_shortcode(  $p->post_content, $shortcode_base ) ){
+            if ( ! has_shortcode( $p->post_content, $shortcode_base ) ) {
                 $p->post_content = '['.$shortcode_base.']'."\r\n \r\n". $p->post_content;
-                wp_update_post(  $p );
+                wp_update_post( $p );
             }
 
-        }else{
+        } else {
             $r = wp_insert_post( $page );
-            if( ! is_wp_error( $r )  && is_numeric( $r ) ){
+            if ( ! is_wp_error( $r ) && is_numeric( $r ) ){
                 $page_id = $r;
             }
         }
 
         $option_keys = array(
-            'st_user_account_page'=> $page_id ,
-            'st_user_disable_default_login'=> 1 ,
-            'st_user_login_redirect_url'=>'',
-            'st_user_logout_redirect_url'=>'',
-            'st_user_term_page'=>'',
+            'st_user_account_page'          => $page_id ,
+            'st_user_disable_default_login' => 1 ,
+            'st_user_login_redirect_url'    => '',
+            'st_user_logout_redirect_url'   => '',
+            'st_user_term_page'             => '',
         );
 
         foreach( $option_keys as $k => $v ){
-            update_option(  $k,  $v );
+            update_option( $k, $v );
         }
 	}
 
